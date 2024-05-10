6f81aed7dc850a2a2a2b273364bb5ba6ba6aa224
Sergio Calahorra <sergiocalahorra20@gmail.com>
2024-05-10 19:48:08 +0200

    Fix incorrect movie id

--- src/app/pages/movie-detail/movie-detail.component.ts
+++ src/app/pages/movie-detail/movie-detail.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { catchError, EMPTY, Subject, takeUntil } from 'rxjs';
 import { MoviesService } from '@services/movies.service';
 import { DetailsListComponent } from '@components/details-list/details-list.component';
 import { MovieDetail } from '@models/movieDetail.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie-detail',
   standalone: true,
-  imports: [DetailsListComponent],
+  imports: [CommonModule, DetailsListComponent],
   templateUrl: './movie-detail.component.html',
   styleUrl: './movie-detail.component.css'
 })
@@ -37,10 +38,15 @@ export class MovieDetailComponent implements OnInit {
 
   private queryData(): void {
     this.moviesService.getMovieDetail(this.movieId)
-      .pipe(takeUntil(this.destroy$))
+      .pipe(
+        takeUntil(this.destroy$),
+        catchError(() => {
+          return EMPTY;
+        }),
+      )
       .subscribe(response => {
         this.movie = response;
-    });
+      });
   }
 
   goToHome(): void {
